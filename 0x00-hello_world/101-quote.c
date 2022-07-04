7b4c8d4cd21455612a49e23514dbdf65d9fc815e
felixtoo48 <felixtoo48@gmail.com>
2022-07-04 11:23:11 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *main - print in standard error
- *Return: 1 if successful
- */
+ * main - print in standard error
+ * Return: 1 if successful
+ *
+ **/
 
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
