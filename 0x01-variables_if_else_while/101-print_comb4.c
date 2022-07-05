46a89c9401e665ec239307382485f864ee31769b
felixtoo48 <felixtoo48@gmail.com>
2022-07-05 02:56:02 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry - point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = (hundreds + 1); tens <= '9'; tens++)
+		{
+			for (ones = (tens + 1); ones <= '9'; ones++)
+			{
+				putchar(hundreds);
+				putchar(tens);
+				putchar(ones);
+				if (hundreds != '7' || tens != '8' || ones != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
