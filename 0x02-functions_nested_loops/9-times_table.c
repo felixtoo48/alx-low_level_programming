6d019c755df1ba6d352256b44648aac658e8a1a6
felixtoo48 <felixtoo48@gmail.com>
2022-07-06 09:47:59 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,14 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int p;
 
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
-			_putchar('i' * 'j');
+			p = (i * j);
+			_putchar(p);
 			_putchar(',');
 			_putchar(' ');
 		}
