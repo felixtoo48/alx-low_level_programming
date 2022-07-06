05f0265e0f28ce6087631434fac968d47e51ab92
felixtoo48 <felixtoo48@gmail.com>
2022-07-06 08:18:43 -0700

    print last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,15 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
+	if (n >= 0)
+	{
+	_putchar('0' + (n % 10));
+	return (n % 10);
+	}
+	else if (n < 0)
 	{
 		n *= -1;
-	}
 	_putchar('0' + (n % 10));
-
 	return (n % 10);
+	}
 }
