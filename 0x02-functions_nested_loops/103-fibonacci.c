97a0c624350aed152c2f1ab0cf0899dda21d9968
felixtoo48 <felixtoo48@gmail.com>
2022-07-06 14:10:36 -0700

    fibonacci 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,13 +11,21 @@ int main(void)
 	int t1 = 0;
 	int t2 = 1;
 	int nextterm = t1 + t2;
+	int sum = 0;
 
 	for (i = 1; i <= 50; i++)
 	{
-		printf("%d, ", nextterm);
 		t1 = t2;
 		t2 = nextterm;
 		nextterm = t1 + t2;
+		while (nextterm <= 4000000)
+		{
+			if (nextterm % 2 == 0)
+			{
+				sum += nextterm;
+				printf("%d\n", sum);
+			}
+		}
 	}
 	return (0);
 }
