6ac48960f06686961aa20424197aa016b51d8e86
felixtoo48 <felixtoo48@gmail.com>
2022-07-06 13:08:15 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints 50 fibonacci numbers
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int t1 = 1;
+	int t2 = 2;
+	int nextterm = t1 + t2;
+
+	for (i = 3; i <= 50; i++)
+	{
+		printf("%d, ", nextterm);
+		t1 = t2;
+		t2 = nextterm;
+		nextterm = t1 + t2;
+	}
+	return (0);
+}
