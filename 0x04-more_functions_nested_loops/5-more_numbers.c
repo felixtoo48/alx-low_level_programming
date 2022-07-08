bd4aa21dcd5a240a094b776aa601ffd47edcf03e
felixtoo48 <felixtoo48@gmail.com>
2022-07-08 04:06:45 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@
 void more_numbers(void)
 {
 	int i;
-	int j;
+	char j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j / 10 > 0);
+			if (j > 9);
 
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
@@ -24,3 +24,5 @@ void more_numbers(void)
 	}
 	_putchar(i);
 }
+
+
