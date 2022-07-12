1ad27bb8014ea973334b73a57d1b2d8e454954df
felixtoo48 <felixtoo48@gmail.com>
2022-07-12 10:55:30 -0700

    elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,16 +6,20 @@
  * @n: number of elements
  */
 
-void print_array(int *a, int n);
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		if (i != (n - 1))
+		{
 			printf("%d, ", a[i]);
+		}
 		else
+		{
 			printf("%d", a[i]);
+		}i
 	}
 	printf("\n");
 }
