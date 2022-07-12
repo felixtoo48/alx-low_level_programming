d188528bfc1a37dd5d4c1ec9acb54abbc231d0f9
felixtoo48 <felixtoo48@gmail.com>
2022-07-12 07:34:09 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: character
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
