9c2d89c172699590cc96850d4796362f0b671798
felixtoo48 <felixtoo48@gmail.com>
2022-07-12 10:16:36 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string entred
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	for (f = '\0'; s[f] != 0; f++)
+	{
+	}
+	r_s = 0;
+	for (f = f - 1; r_s < f; r_s++)
+	{
+		character = s[f];
+		s[f] = s[r_s];
+		s[r_s] = character;
+		f--;
+	}
+}
