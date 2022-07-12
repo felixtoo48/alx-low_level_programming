9980a33b331d16e02b5495f2d70f2022709cc203
felixtoo48 <felixtoo48@gmail.com>
2022-07-12 11:28:38 -0700

    strcopy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ *  * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
  * _strcpy - copies string pointed by src
  * @dest: copy source to this buffer
  * @src: this is the source to copy
