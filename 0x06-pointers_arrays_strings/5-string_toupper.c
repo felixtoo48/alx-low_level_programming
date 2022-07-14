28ace5dba4efb1f75d9ff454bfe8797521fae8ae
felixtoo48 <felixtoo48@gmail.com>
2022-07-14 14:16:36 -0700

    lower case to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower case letters to uppercase
+ * @s: string to manipulate
+ * Returned: string with capital letters
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
+	}
+	return (s);
+}
