ca9938c93cf6d2d53ff6f91df476590e8848571b
felixtoo48 <felixtoo48@gmail.com>
2022-07-14 13:24:47 -0700

    concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int d;
-	int s;
+	int i = 0;
+	int j = 0;
 
-	while (dest[d] != '\0')
-		d++;
-	while (src[s] != '\0' && s < n)
+	while (dest[i] != '\0') /*i is index of null terminator*/
+		i++;
+	while (src[j] != src[n]) /*append replacing null terminator*/
 	{
-		dest[d] = src[s];
-		d++;
-		s++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
