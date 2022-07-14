17446fa62566eae837b492d1a952e64d5d6f7871
felixtoo48 <felixtoo48@gmail.com>
2022-07-14 14:48:31 -0700

    capitalization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,7 @@
  * Return: string
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 0;
 
@@ -21,7 +21,7 @@ char *cap_string(char *)
 
 		/* if lowercase and prior char is separator, capitalize*/
 		if ((s[i] >= 'a' && s[i] <= 'z')
-			 && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+		 && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
 				s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
 				s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
 				s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
