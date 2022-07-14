000055ae3d3735cb0ebe84c65792572d3e3f3a90
felixtoo48 <felixtoo48@gmail.com>
2022-07-14 11:25:12 -0700

    concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,23 @@
 
 /**
  * *_strcat - concatenates two strings
- * @*dest:
- * @*src:
- * Return: nothing
+ * @*dest: destination string to append source string to
+ * @*src: source string
+ * Return: dest string
  */
 
 char *_strcat(char *dest, char *src)
 {
+	int d = 0;
+	int s = 0;
 
+	while (dest[d] != '\0')
+		d++;
+	while (src[s] != '\0')
+	{
+		dest[d] = src[s];
+		d++;
+		s++;
+	}
+	return (dest);
+}
