0f088b01f645d8ae9d4c1a2f38762b415ea09c66
felixtoo48 <felixtoo48@gmail.com>
2022-07-22 11:08:39 -0700

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply entered numbers
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
