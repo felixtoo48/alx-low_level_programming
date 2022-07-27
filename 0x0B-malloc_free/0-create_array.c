db12362c60fa1c06b631544e9be01ca0fb5c1265
felixtoo48 <felixtoo48@gmail.com>
2022-07-27 04:23:00 -0700

    creates array of characters and initializes it to a specific character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,21 +10,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *p;
-
-	if (size == 0) /* validate size input */
-		return (0);
-	
-	p = malloc(sizeof(char) * size); /* allocate memory */
-
-	if (p == NULL) /* validate memory */
+	char *m = malloc(size);
+	if (size == 0 || m == 0)
 		return (0);
 
-	while (i < size) /* set array values to char c */
-	{
-		p[i] = c;
-		i++;
-	}
-	return (p);
+	while (size--)
+		m[size] = c;
+
+	return (m);
 }
